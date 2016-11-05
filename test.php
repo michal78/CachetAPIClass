70ccb2237ae36d1188f360863b4dd34e69b08712
Michal Skogemann <michal.skogemann@gmail.com>
2016-11-05 22:53:59 +0100

    Update test.php
    Minor adjustments

--- test.php
+++ test.php
@@ -1,12 +1,9 @@
 <?php
 ## Just proof of concept
 require_once 'cachet.class.php';
-echo '<pre>';
 
 $token = 'TheRandomTokenFromYourProfile';
 $host = 'your.cachethost.tld';
 
-$cachet = new cachet($host, $token, false);
-$components = $cachet->components();
-
-echo '</pre>';
+$cachet = new cachet($host, $token, false); // Last parameter is debug true/false
+$components = $cachet->components(); // Components as an PHP object
